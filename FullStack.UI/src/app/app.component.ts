40eece5986c793a5588d56a66bd21d05205c532b
CristianoCap <cristiano.antonio.capanna@gmail.com>
2023-03-16 17:43:01 +0100

    001-commit
    - EmployeesListComponent;
    - Employee Interface;
    - Visualization of Employees List

--- FullStack.UI/src/app/app.component.ts
+++ FullStack.UI/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './model/employee.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'FullStack.UI';
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
 }
