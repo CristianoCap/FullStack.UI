08be24ae4395fc14c8cfca247cc6ee8431e17036
CristianoCap <cristiano.antonio.capanna@gmail.com>
2023-03-16 22:08:31 +0100

    003-commit
    - Fix error on EmployeesListComponent *ngFor

--- FullStack.UI/src/app/component/employees-list/employees-list.component.ts
+++ FullStack.UI/src/app/component/employees-list/employees-list.component.ts
@@ -9,9 +9,9 @@ import { Employee } from 'src/app/model/employee.model';
 export class EmployeesListComponent implements OnInit {
 
   employees: Employee[] = [
-    /*{id: '001', name: 'Cristiano Capanna', email: 'cristiano.capanna@gmail.com', phone: 111111111, salary: 2000, department: 'IT'},
+    {id: '001', name: 'Cristiano Capanna', email: 'cristiano.capanna@gmail.com', phone: 111111111, salary: 2000, department: 'IT'},
     {id: '002', name: 'Cristiana Silvestri', email: 'cristiana.silvestri@libero.com', phone: 222222222, salary: 2200, department: 'HR'},
-    {id: '003', name: 'Antonio Campana', email: 'anotnio.campana@hotmail.com', phone: 333333333, salary: 1900, department: 'BSN'}*/
+    {id: '003', name: 'Antonio Campana', email: 'anotnio.campana@hotmail.com', phone: 333333333, salary: 1900, department: 'BSN'}
   ]
 
   constructor() { }
